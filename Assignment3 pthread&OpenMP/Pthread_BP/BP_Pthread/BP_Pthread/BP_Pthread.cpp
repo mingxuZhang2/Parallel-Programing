f9decc67bf5986ee79e872d12df39518ed8ebab3
张铭徐 <116150363+mingxuZhang2@users.noreply.github.com>
2023-04-25 20:13:49 +0800

    Add files via upload

---
+++ Assignment3 pthread&OpenMP/Pthread_BP/BP_Pthread/BP_Pthread/BP_Pthread.cpp
@@ -0,0 +1,393 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <pthread.h>
+#pragma comment(lib, "pthreadVC2.lib")
+using namespace std;
+
+// Sigmoid激活函数及其导数
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+
+double sigmoid_derivative(double x) {
+    double s = sigmoid(x);
+    return s * (1 - s);
+}
+
+struct BPData {
+    const vector<vector<double>>* inputs;
+    const vector<vector<double>>* targets;
+    vector<vector<double>>* output_weights;
+    vector<vector<double>>* hidden_weights;
+    vector<double>* output_bias;
+    vector<double>* hidden_bias;
+    int start;
+    int end;
+};
+
+void* bp_thread(void* arg) {
+    BPData* data = static_cast<BPData*>(arg);
+    const vector<vector<double>>& inputs = *data->inputs;
+    const vector<vector<double>>& targets = *data->targets;
+    vector<vector<double>>& output_weights = *data->output_weights;
+    vector<vector<double>>& hidden_weights = *data->hidden_weights;
+    vector<double>& output_bias = *data->output_bias;
+    vector<double>& hidden_bias = *data->hidden_bias;
+    int start = data->start;
+    int end = data->end;
+
+    double learning_rate = 0.1;
+
+    for (int i = start; i < end; ++i) {
+        const vector<double>& input = inputs[i];
+        const vector<double>& target = targets[i];
+
+        int input_size = input.size();
+        int hidden_size = hidden_weights.size();
+        int output_size = output_weights.size();
+
+        // 前向传播
+        vector<double> hidden(hidden_size);
+        for (int j = 0; j < hidden_size; ++j) {
+            double z = 0;
+            for (int k = 0; k < input_size; ++k) {
+                z += input[k] * hidden_weights[j][k];
+            }
+            z += hidden_bias[j];
+            hidden[j] = sigmoid(z);
+        }
+
+        vector<double> output(output_size);
+        for (int j = 0; j < output_size; ++j) {
+            double z = 0;
+            for (int k = 0; k < hidden_size; ++k) {
+                z += hidden[k] * output_weights[j][k];
+            }
+            z += output_bias[j];
+            output[j] = sigmoid(z);
+        }
+
+        // 计算输出层和隐藏层的误差
+        vector<double> output_error(output_size);
+        vector<double> hidden_error(hidden_size);
+        for (int j = 0; j < output_size; ++j) {
+            output_error[j] = (target[j] - output[j]) * sigmoid_derivative(output[j]);
+        }
+        for (int j = 0; j < hidden_size; ++j) {
+            double error_sum = 0;
+            for (int k = 0; k < output_size; ++k) {
+                error_sum += output_error[k] * output_weights[k][j];
+            }
+            hidden_error[j] = error_sum * sigmoid_derivative(hidden[j]);
+        }
+        // 更新权重和偏置
+        for (int j = 0; j < output_size; ++j) {
+            for (int k = 0; k < hidden_size; ++k) {
+                output_weights[j][k] += learning_rate * output_error[j] * hidden[k];
+            }
+            output_bias[j] += learning_rate * output_error[j];
+        }
+        for (int j = 0; j < hidden_size; ++j) {
+            for (int k = 0; k < input_size; ++k) {
+                hidden_weights[j][k] += learning_rate * hidden_error[j] * input[k];
+            }
+            hidden_bias[j] += learning_rate * hidden_error[j];
+        }
+    }
+
+    return nullptr;
+}
+
+int main() {
+    int input_size = 30000;
+    int hidden_size = 2000;
+    int output_size = 2;
+    int num_samples = 100;
+    int num_threads = 4;// 使用随机数生成器生成训练数据
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-1, 1);
+
+    vector<vector<double>> inputs(num_samples, vector<double>(input_size));
+    vector<vector<double>> targets(num_samples, vector<double>(output_size));
+
+    for (auto& sample : inputs) {
+        for (auto& elem : sample) {
+            elem = dis(gen);
+        }
+    }
+
+    for (auto& sample : targets) {
+        for (auto& elem : sample) {
+            elem = dis(gen);
+        }
+    }
+
+    // 初始化权重和偏置
+    vector<vector<double>> output_weights(output_size, vector<double>(hidden_size));
+    vector<vector<double>> hidden_weights(hidden_size, vector<double>(input_size));
+    vector<double> output_bias(output_size);
+    vector<double> hidden_bias(hidden_size);
+
+    for (auto& row : output_weights) {
+        for (auto& elem : row) {
+            elem = dis(gen);
+        }
+    }
+
+    for (auto& row : hidden_weights) {
+        for (auto& elem : row) {
+            elem = dis(gen);
+        }
+    }
+
+    for (auto& elem : output_bias) {
+        elem = dis(gen);
+    }
+
+    for (auto& elem : hidden_bias) {
+        elem = dis(gen);
+    }
+
+    // 训练神经网络
+    int epochs = 10;
+
+    vector<pthread_t> threads(num_threads);
+    vector<BPData> thread_data(num_threads);
+
+    int chunk_size = num_samples / num_threads;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int epoch = 0; epoch < epochs; ++epoch) {
+        for (int i = 0; i < num_threads; ++i) {
+            thread_data[i].inputs = &inputs;
+            thread_data[i].targets = &targets;
+            thread_data[i].output_weights = &output_weights;
+            thread_data[i].hidden_weights = &hidden_weights;
+            thread_data[i].output_bias = &output_bias;
+            thread_data[i].hidden_bias = &hidden_bias;
+            thread_data[i].start = i * chunk_size;
+            thread_data[i].end = (i == num_threads - 1) ? num_samples : (i + 1) * chunk_size;
+
+            pthread_create(&threads[i], nullptr, bp_thread, &thread_data[i]);
+        }
+
+        for (int i = 0; i < num_threads; ++i) {
+            pthread_join(threads[i], nullptr);
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "训练耗时: " << elapsed.count() << " 秒" << std::endl;
+    return 0;
+}
+//#include <iostream>
+//#include <vector>
+//#include <cmath>
+//#include <random>
+//#include <chrono>
+//#include <thread>
+//#include <mutex>
+//#include <condition_variable>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//// Sigmoid激活函数及其导数
+//double sigmoid(double x) {
+//    return 1.0 / (1.0 + exp(-x));
+//}
+//
+//double sigmoid_derivative(double x) {
+//    double s = sigmoid(x);
+//    return s * (1 - s);
+//}
+//
+//struct BPData {
+//    const vector<vector<double>>* inputs;
+//    const vector<vector<double>>* targets;
+//    vector<vector<double>>* output_weights;
+//    vector<vector<double>>* hidden_weights;
+//    vector<double>* output_bias;
+//    vector<double>* hidden_bias;
+//    int start;
+//    int end;
+//    mutex* mtx;
+//    condition_variable* cv;
+//    bool* finished;
+//};
+//
+//void bp_thread(BPData* data) {
+//    const vector<vector<double>>& inputs = *data->inputs;
+//    const vector<vector<double>>& targets = *data->targets;
+//    vector<vector<double>>& output_weights = *data->output_weights;
+//    vector<vector<double>>& hidden_weights = *data->hidden_weights;
+//    vector<double>& output_bias = *data->output_bias;
+//    vector<double>& hidden_bias = *data->hidden_bias;
+//    int start = data->start;
+//    int end = data->end;
+//
+//    double learning_rate = 0.1;
+//
+//    for (int i = start; i < end; ++i) {
+//        const vector<double>& input = inputs[i];
+//        const vector<double>& target = targets[i];
+//
+//        int input_size = input.size();
+//        int hidden_size = hidden_weights.size();
+//        int output_size = output_weights.size();
+//
+//        // 前向传播
+//        vector<double> hidden(hidden_size);
+//        for (int j = 0; j < hidden_size; ++j) {
+//            double z = 0;
+//            for (int k = 0; k < input_size; ++k) {
+//                z += input[k] * hidden_weights[j][k];
+//            }
+//            z += hidden_bias[j];
+//            hidden[j] = sigmoid(z);
+//        }
+//
+//        vector<double> output(output_size);
+//        for (int j = 0; j < output_size; ++j) {
+//            double z = 0;
+//            for (int k = 0; k < hidden_size; ++k) {
+//                z += hidden[k] * output_weights[j][k];
+//            }
+//            z += output_bias[j];
+//            output[j] = sigmoid(z);
+//        }
+//        // 计算输出层和隐藏层的误差
+//        vector<double> output_error(output_size);
+//        vector<double> hidden_error(hidden_size);
+//        for (int j = 0; j < output_size; ++j) {
+//            output_error[j] = (target[j] - output[j]) * sigmoid_derivative(output[j]);
+//        }
+//        for (int j = 0; j < hidden_size; ++j) {
+//            double error_sum = 0;
+//            for (int k = 0; k < output_size; ++k) {
+//                error_sum += output_error[k] * output_weights[k][j];
+//            }
+//            hidden_error[j] = error_sum * sigmoid_derivative(hidden[j]);
+//        }
+//
+//        // 更新权重和偏置
+//        lock_guard<mutex> lock(*data->mtx);
+//        for (int j = 0; j < output_size; ++j) {
+//            for (int k = 0; k < hidden_size; ++k) {
+//                output_weights[j][k] += learning_rate * output_error[j] * hidden[k];
+//            }
+//            output_bias[j] += learning_rate * output_error[j];
+//        }
+//        for (int j = 0; j < hidden_size; ++j) {
+//            for (int k = 0; k < input_size; ++k) {
+//                hidden_weights[j][k] += learning_rate * hidden_error[j] * input[k];
+//            }
+//            hidden_bias[j] += learning_rate * hidden_error[j];
+//        }
+//    }
+//
+//    {
+//        lock_guard<mutex> lock(*data->mtx);
+//        *data->finished = true;
+//    }
+//    data->cv->notify_all();
+//}
+//
+//int main() {
+//    int input_size = 3;
+//    int hidden_size = 4;
+//    int output_size = 2;
+//    int num_samples = 100;
+//    int num_threads = thread::hardware_concurrency();
+//    // 使用随机数生成器生成训练数据
+//    std::random_device rd;
+//    std::mt19937 gen(rd());
+//    std::uniform_real_distribution<> dis(-1, 1);
+//    vector<vector<double>> inputs(num_samples, vector<double>(input_size));
+//    vector<vector<double>> targets(num_samples, vector<double>(output_size));
+//
+//    for (auto& sample : inputs) {
+//        for (auto& elem : sample) {
+//            elem = dis(gen);
+//        }
+//    }
+//
+//    for (auto& sample : targets) {
+//        for (auto& elem : sample) {
+//            elem = dis(gen);
+//        }
+//    }
+//
+//    // 初始化权重和偏置
+//    vector<vector<double>> output_weights(output_size, vector<double>(hidden_size));
+//    vector<vector<double>> hidden_weights(hidden_size, vector<double>(input_size));
+//    vector<double> output_bias(output_size);
+//    vector<double> hidden_bias(hidden_size);
+//
+//    for (auto& row : output_weights) {
+//        for (auto& elem : row) {
+//            elem = dis(gen);
+//        }
+//    }
+//
+//    for (auto& row : hidden_weights) {
+//        for (auto& elem : row) {
+//            elem = dis(gen);
+//        }
+//    }
+//
+//    for (auto& elem : output_bias) {
+//        elem = dis(gen);
+//    }
+//
+//    for (auto& elem : hidden_bias) {
+//        elem = dis(gen);
+//    }
+//
+//    // 训练神经网络
+//    int epochs = 1000;
+//    int chunk_size = num_samples / num_threads;
+//    vector<thread> threads(num_threads);
+//    vector<BPData> thread_data(num_threads);
+//    mutex mtx;
+//    condition_variable cv;
+//    vector<char>thread_finished(num_threads, false);
+//    auto start = std::chrono::high_resolution_clock::now();
+//
+//    for (int epoch = 0; epoch < epochs; ++epoch) {
+//        for (int i = 0; i < num_threads; ++i) {
+//            thread_data[i].inputs = &inputs;
+//            thread_data[i].targets = &targets;
+//            thread_data[i].output_weights = &output_weights;
+//            thread_data[i].hidden_weights = &hidden_weights;
+//            thread_data[i].output_bias = &output_bias;
+//            thread_data[i].hidden_bias = &hidden_bias;
+//            thread_data[i].start = i * chunk_size;
+//            thread_data[i].end = (i == num_threads - 1) ? num_samples : (i + 1) * chunk_size;
+//            thread_data[i].mtx = &mtx;
+//            thread_data[i].cv = &cv;
+//            thread_data[i].finished = &thread_finished[i];
+//
+//            threads[i] = thread(bp_thread, &thread_data[i]);
+//        }
+//
+//        {
+//            unique_lock<mutex> lock(mtx);
+//            cv.wait(lock, [&thread_finished] {
+//                return all_of(thread_finished.begin(), thread_finished.end(), [](char f) { return f; });
+//                });
+//            fill(thread_finished.begin(), thread_finished.end(), false);
+//        }
+//    }
+//
+//    auto end = std::chrono::high_resolution_clock::now();
+//    std::chrono::duration<double> elapsed = end - start;
+//    std::cout << "训练耗时: " << elapsed.count() << " 秒" << std::endl;
+//    return 0;
+//
+//}
---
+++ Assignment3 pthread&OpenMP/pthread_avx_conv/pthread_avx_conv/pthread_avx_conv.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <vector>
+#include <immintrin.h>
+#include <chrono>
+#include <random>
+#include <pthread.h>
+#pragma comment(lib, "pthreadVC2.lib")
+using namespace std;
+
+struct ConvolutionData {
+    const vector<float>* signal;
+    const vector<float>* kernel;
+    vector<float>* result;
+    int start;
+    int end;
+    double elapsed_time; // 新增：存储每个线程的用时
+};
+
+void* convolution_thread(void* arg) {
+    auto thread_start = std::chrono::high_resolution_clock::now();
+    ConvolutionData* data = static_cast<ConvolutionData*>(arg);
+    const vector<float>& signal = *data->signal;
+    const vector<float>& kernel = *data->kernel;
+    vector<float>& result = *data->result;
+    int signal_length = signal.size();
+    int kernel_length = kernel.size();
+    int kernel_aligned_length = (kernel_length + 7) / 8 * 8;
+
+    __m256 signal_reg, kernel_reg, mul_reg, add_reg;
+
+    for (int i = data->start; i < data->end; ++i) {
+        for (int j = 0; j < kernel_aligned_length; j += 8) {
+            int result_idx = i + j;
+            if (j < kernel_length) {
+                signal_reg = _mm256_set1_ps(signal[i]);
+                kernel_reg = _mm256_loadu_ps(&kernel[j]);
+                mul_reg = _mm256_mul_ps(signal_reg, kernel_reg);
+                add_reg = _mm256_loadu_ps(&result[result_idx]);
+                add_reg = _mm256_add_ps(add_reg, mul_reg);
+                _mm256_storeu_ps(&result[result_idx], add_reg);
+            }
+        }
+    }
+    auto thread_end = std::chrono::high_resolution_clock::now(); // 新增：线程结束计时
+
+    data->elapsed_time = std::chrono::duration<double>(thread_end - thread_start).count(); // 新增：计算并存储线程用时
+    return nullptr;
+}
+
+
+int main() {
+    int signal_length = 100000;
+    int kernel_length = 8000;
+    int num_threads = 4;
+    double tim[50] = { 0.0 };
+    vector<float> signal(signal_length);
+    vector<float> kernel(kernel_length);
+
+    // 随机数生成器
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(-10, 10);
+
+    // 生成随机信号
+    for (auto& elem : signal) {
+        elem = dis(gen);
+    }
+
+    // 生成随机卷积核
+    for (auto& elem : kernel) {
+        elem = dis(gen);
+    }
+
+    int result_length = signal_length + kernel_length - 1;
+    vector<float> result(result_length, 0);
+
+    // 创建线程和线程数据
+    vector<pthread_t> threads(num_threads);
+    vector<ConvolutionData> thread_data(num_threads);
+
+    int chunk_size = signal_length / num_threads;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    // 创建并运行线程
+    for (int i = 0; i < num_threads; ++i) {
+        thread_data[i].signal = &signal;
+        thread_data[i].kernel = &kernel;
+        thread_data[i].result = &result;
+        thread_data[i].start = i * chunk_size;
+        thread_data[i].end = (i == num_threads - 1) ? signal_length : (i + 1) * chunk_size;
+
+        pthread_create(&threads[i], nullptr, convolution_thread, &thread_data[i]);
+    }
+
+    // 等待所有线程完成
+    for (int i = 0; i < num_threads; ++i) {
+        pthread_join(threads[i], nullptr);
+        tim[i] += thread_data[i].elapsed_time;
+    }
+    for (int i = 0; i < num_threads; i++) {
+        std::cout << "线程 " << i << "用时" << tim[i] << endl;
+
+    }
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout << "计算耗时: " << elapsed.count() << " 秒" << endl;
+
+    system("pause");
+    return 0;
+}
